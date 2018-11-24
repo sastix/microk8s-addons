ba39a54859e8819d385ed60a75f6691ac3fe0b7f
John Buluba <buluba89@gmail.com>
2018-11-24 19:40:16 +0200

    Converted to observable

--- microk8s-webconsole/server/src/addons/addons.service.ts
+++ microk8s-webconsole/server/src/addons/addons.service.ts
@@ -3,19 +3,25 @@ import { execFile } from 'child_process';
 import { promisify } from 'util';
 import { safeLoad } from 'js-yaml';
 import { Addon } from '@common/models/addon.interface';
+import { from, observable } from 'rxjs';
+import { flatMap, map } from 'rxjs/operators';
 
 const exec_promise = promisify(execFile);
 
 @Injectable()
 export class AddonsService {
   async getAll(): Promise<Addon[]> {
-    const response = await exec_promise('microk8s.status',  ['--yaml']);
-    const parsed = safeLoad(response.stdout);
-    const addonsList = [];
-    for (const addon in parsed.addons) {
-      const isEnabled: boolean = parsed.addons[addon] === 'enabled';
-      addonsList.push({name: addon, enabled: isEnabled});
-    }
-    return addonsList;
+    return await from(exec_promise('microk8s.status',  ['--yaml'])).pipe(
+      map(r => safeLoad(r.stdout).addons),
+      map(addons => {
+        const values: Addon[] = [];
+        for (const key in addons){
+          if (addons.hasOwnProperty(key)){
+            values.push({name: key, enabled: addons[key]});
+          }
+        }
+        return values;
+      }),
+    ).toPromise();
   }
 }
