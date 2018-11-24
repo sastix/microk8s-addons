bfa857a5316b148cc25acfefcea31962f779d675
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2018-11-24 20:41:23 +0200

    Add power service

--- microk8s-webconsole/server/src/app.module.ts
+++ microk8s-webconsole/server/src/app.module.ts
@@ -5,10 +5,13 @@ import { AddonsModule } from './addons/addons.module';
 import { SnapController } from '../src/snap/snap.controller';
 import { SnapService } from '../src/snap/snap.service';
 import {ShellService} from "./core/services/shell/shell.service";
+import { PowerController } from './power/power.controller';
+import { PowerService } from './power/power.service';
+import { PowerModule } from './power/power.module';
 
 @Module({
-  imports: [AddonsModule],
-  controllers: [AppController, SnapController],
-  providers: [AppService, SnapService, ShellService],
+  imports: [AddonsModule, PowerModule],
+  controllers: [AppController, PowerController, SnapController],
+  providers: [AppService, PowerService, SnapService, ShellService],
 })
 export class AppModule {}
