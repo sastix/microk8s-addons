c537160d084b3c014f9ad5c68aa7760b70b43be8
Stavros Grigoriou <unix121@protonmail.com>
2019-01-02 14:17:30 +0200

    Fix for start.sh script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env bash
+#/usr/bin/env bash
 
-(cd microk8s-webconsole/server; sudo npm run start:dev &> server.output &)
-(cd microk8s-webconsole/ui; npm run start &> ui.output &)
+PROJECT_HOME="$(pwd)"
+read -p "Root password: " -s ROOT_PASSWORD
+
+(cd $PROJECT_HOME/../microk8s-webconsole/server; echo $ROOT_PASSWORD | sudo -S npm run start:dev &> server.output &)
+(cd $PROJECT_HOME/../microk8s-webconsole/ui; npm run start &> ui.output &)
 
 echo "Point your browser at http://localhost:4200"
