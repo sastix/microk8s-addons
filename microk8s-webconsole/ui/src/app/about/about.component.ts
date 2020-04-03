e7d6cc96f57afdf5fe986837a833d5ba5b9616f1
iskitsas <iskitsas@gmail.com>
2020-04-03 17:15:45 +0300

    Fix About/MicroK8s version not showing properly

--- microk8s-webconsole/ui/src/app/about/about.component.ts
+++ microk8s-webconsole/ui/src/app/about/about.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../core/api.service';
 import {Observable} from 'rxjs';
+import {map} from "rxjs/operators";
+import {HttpResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-about',
@@ -16,7 +18,10 @@ export class AboutComponent implements OnInit {
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
-    this.mversion$ = this.apiService.post('version', {callback: 'xyztoken'}, null, 'text');
+    this.mversion$ = this.apiService.post('version', {callback: 'xyztoken'}, null, 'text')
+      .pipe(
+        map((r: HttpResponse<Object>) => r.body.toString())
+      );
   }
 
 }
