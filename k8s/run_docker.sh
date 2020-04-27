a7e4489479ab9e6f049f01f722fe7590aeecfc3d
iskitsas <iskitsas@gmail.com>
2020-04-27 19:02:18 +0300

    Improve nginx conf template and have run script for docker providing output to console

--- k8s/run_docker.sh
+++ k8s/run_docker.sh
@@ -10,7 +10,7 @@ then
     exit
 fi
 
-CMD="docker run -p 80:80 -e CALLBACK_TOKEN=xyztoken -e K8S_TOKEN="${K8S_TOKEN}" -e HOST_ACCESS_IP=10.0.2.2 -e K8S_USER=microadmin -e K8S_USER_PASS=\$apr1\$029KDYcF\$OWgQy3KmUTum5je0T1Ooi1 sastix/microdash:1.0${1}"
+CMD="docker run -p 80:80 -e CALLBACK_TOKEN=xyztoken -e K8S_TOKEN="${K8S_TOKEN}" -e HOST_ACCESS_IP=10.0.2.2 -e K8S_USER='microadmin' -e K8S_USER_PASS='\$apr1\$029KDYcF\$OWgQy3KmUTum5je0T1Ooi1' sastix/microdash:1.0${1}"
 echo $CMD
 
-echo $($CMD)
+eval $CMD
